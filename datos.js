49d0f6622ed43aa38a06b2f8bcc59ff5c28f1c23
venezolana <venezolanaseguros@gmail.com>
2025-11-21 01:03:47 -0400

    FORZANDO SUBIDA FINAL DE STYLES.CSS

--- datos.js
+++ datos.js
@@ -1,4 +1,4 @@
 const baseDeDatos = {
-  "70226": {    carga: "pesada",    vehiculo: {      codigo: "8123n1m",      tipo: "MOTO",      placa: "0",      carroceria: "Paseo",      color: "Amarillo",      ejes: "2"    },    carnet: {      placa: "ad1e31k",      marca: "KEEWAY",      modelo: "Rkv 200",      anio: "2013"    },    pdfContrato: "documentos/57695 CARLOS Venezolana_seguros.pdf",    pdfCarnet: "documentos/57695 CARLOS Carnet.pdf"  },
+  "70226": {    carga: "pesada",    vehiculo: {      codigo: "8123n1m",      tipo: "MOTO",      placa: "0",      carroceria: "Paseo",      color: "Amarillo",      ejes: "2"    },    carnet: {      placa: "ad1e31k",      marca: "KEEWAY",      modelo: "Rkv 200",      anio: "2013"    },    pdfContrato: "documentos/57695 CARLOS Venezolana_seguros.pdf",    pdfCarnet: "CONTRATOS/57695 CARLOS Carnet.pdf"  },
   "2345": {    carga: "livianaa",    vehiculo: {      codigo: "23",      tipo: "MOTO",      placa: "B2",      carroceria: "D2",      color: "VERDE",      ejes: "2"    },    carnet: {      placa: "PLACA2",      marca: "KEEWAY",      modelo: "EMPIRE",      anio: "2010"    },    pdfContrato: "",    pdfCarnet: ""  }
 };
