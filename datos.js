a0f8fa239c52acdadd864b35eb264913abc1122a
venezolana <venezolanaseguros@gmail.com>
2025-11-21 01:59:59 -0400

    ACTUALIZACION

--- datos.js
+++ datos.js
@@ -1,4 +1,3 @@
 const baseDeDatos = {
-  "70226": {    carga: "pesada",    vehiculo: {      codigo: "8123n1m",      tipo: "MOTO",      placa: "0",      carroceria: "Paseo",      color: "Amarillo",      ejes: "2"    },    carnet: {      placa: "ad1e31k",      marca: "KEEWAY",      modelo: "Rkv 200",      anio: "2013"    },    pdfContrato: "CONTRATOS/57695 CARLOS Venezolana_seguros.pdf",    pdfCarnet: "CONTRATOS/57695 CARLOS Carnet.pdf"  },
-  "2345": {    carga: "livianaa",    vehiculo: {      codigo: "23",      tipo: "MOTO",      placa: "B2",      carroceria: "D2",      color: "VERDE",      ejes: "2"    },    carnet: {      placa: "PLACA2",      marca: "KEEWAY",      modelo: "EMPIRE",      anio: "2010"    },    pdfContrato: "",    pdfCarnet: ""  }
+  "057695": {    carga: "MOTOCICLETA",    vehiculo: {      codigo: "812K3AC11CM057776",      tipo: "MOTO",      placa: "0",      carroceria: "Paseo",      color: "ROJO",      ejes: "2"    },    carnet: {      placa: "AA3M75J",      marca: "KEEWAY",      modelo: "HORSE KW-150",      anio: "2012"    },    pdfContrato: "CONTRATOS/57695 CARLOS Venezolana_seguros.pdf",    pdfCarnet: "CONTRATOS/57695 CARLOS Carnet.pdf"  }
 };
